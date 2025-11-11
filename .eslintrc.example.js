66e34b9b98f1e36959bbbab60c1648f7ddcb9b30
Lucas Broda <lulubrod@gmail.com>
2025-11-11 17:08:31 +0100

    build: ajout de config

---
+++ .eslintrc.example.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@react-native-community',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  rules: {
+    // React Hooks
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    
+    // TypeScript
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    
+    // Code Style
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'prefer-const': 'error',
+    'no-var': 'error',
+    
+    // React
+    'react/prop-types': 'off', // Using TypeScript for prop validation
+    'react/react-in-jsx-scope': 'off', // Not needed in React 17+
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,23 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+          alias: {
+            "@": "./src",
+            "@components": "./src/components",
+            "@features": "./src/features",
+            "@hooks": "./src/hooks",
+            "@services": "./src/services",
+            "@utils": "./src/utils",
+            "@constants": "./src/constants",
+            "@types": "./src/types"
+          }
+        }
+      ]
+    ]
   };
 };
